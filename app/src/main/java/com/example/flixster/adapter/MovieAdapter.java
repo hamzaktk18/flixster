659209693c02b3ce61e6f595f18dc382fde7dd1f
khattakm <mhamzaktkcs@gmail.com>
2020-02-02 22:30:14 -0500

    Flixster 2

--- app/src/main/java/com/example/flixster/adapter/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapter/MovieAdapter.java
@@ -1,27 +1,29 @@
 package com.example.flixster.adapter;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.bumptech.glide.Glide;
 import com.example.flixster.R;
+import com.example.flixster.models.DetailActivity;
 import com.example.flixster.models.Movie;
 
+import org.parceler.Parcels;
 import org.w3c.dom.Text;
-
 import java.util.List;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
-
 
     Context context;
     List<Movie> movies;
@@ -49,7 +51,6 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
         // bind the movie data into the VH
         holder.bind(movie);
-
     }
 
     // returns the total count of items in the list
@@ -60,23 +61,41 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
-
 
         public ViewHolder(View itemView) {
             super(itemView);
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText((movie.getTitle()));
             tvOverview.setText(movie.getOverview());
             Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+//            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+//                imageUrl = movie.getBackdropPath();
+//            } else {
+//                imageUrl = movie.getPosterPath();
+//            }
+//            Glide.with(context).load(ImageUrl).into(ivPoster);
+
+            // 1. register click listener on the whole row
+
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // 2. when tapped, go to a new activity
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -3,20 +3,29 @@ package com.example.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
     String posterPath;
     String title;
     String overview;
+    Double rating;
+
+    // empty constructor needed by the Parceler library
+    public Movie(){
+
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath= jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
 
     }
 
@@ -38,5 +47,9 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public Double getRating() {
+        return rating;
     }
 }
